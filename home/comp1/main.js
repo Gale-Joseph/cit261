9b12cecb2a33d28ee8dd1e39562ba86dd2588fce
Joseph Gale <gal16026@byui.edu>
2019-10-05 23:55:59 -0500

    update

---
+++ home/comp1/main.js
@@ -0,0 +1,14 @@
+/* JavaScript for Competency 1 Notes: 
+Data Types: 
+undefined, null, boolean, string, symbol, number, objects
+*/
+
+//use var or any datatypes for whole program
+var myName = "Joe";
+
+//let just for scope
+let sisterName = "Sarah";
+
+//const for immutable variables
+const pi = 3.41;
+console.log(pi);
