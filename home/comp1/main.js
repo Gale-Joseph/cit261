45d8a30d8e7f7ba4ca21a8f1c13e107ce0470750
Joseph Gale <gal16026@byui.edu>
2019-10-07 07:59:41 -0500

    car inventory

--- home/comp1/main.js
+++ home/comp1/main.js
@@ -1,14 +1,44 @@
-/* JavaScript for Competency 1 Notes: 
-Data Types: 
-undefined, null, boolean, string, symbol, number, objects
-*/
+var cars = ["Saab","Volvo","BMW"];
+function checkMyCar(){
+    var carCheck = parseInt(document.getElementById("carNum").value);
+    if(carCheck>cars.length-1){
+        document.getElementById("checkCarDiv").innerHTML="Number too high." +
+        "Choose a number less than "+ String(cars.length) + ".";
+    }else{
+    document.getElementById("checkCarDiv").innerHTML=cars[carCheck];
+    }
+}
 
-//use var or any datatypes for whole program
-var myName = "Joe";
+function seeCarTotal(){
+    document.getElementById("totalCarsOutput").innerHTML="There are " + 
+    String(cars.length) + " cars on the list."
+}
 
-//let just for scope
-let sisterName = "Sarah";
 
-//const for immutable variables
-const pi = 3.41;
-console.log(pi);
+//print out array
+function printArray(){
+    document.getElementById("inventoryOutput").innerHTML=cars.join(" ");
+}
+
+//search with for loop
+function checkByCar(){
+    var carInQuestion = document.getElementById("carCheck").value;
+    console.log("var established");
+    for(var i=0;i<cars.length;i++){
+        if(cars[i].toLowerCase==carInQuestion.toLowerCase){
+            document.getElementById("checkByCarOutput").innerHTML = carInQuestion + " has been found."
+            break;
+        }else{
+            document.getElementById("checkByCarOutput").innerHTML = carInQuestion + " has not been found."
+
+        }
+    }
+}
+
+//while loop
+
+//switch statement
+
+//associate array
+
+//conditional statements
