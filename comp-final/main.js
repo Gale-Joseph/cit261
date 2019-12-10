382b5d2c1cafd1c16d21f92b05eb6cad5acc7ee9
Joseph Gale <gal16026@byui.edu>
2019-12-10 06:34:58 -0600

    Tuesday project upload

--- comp-final/main.js
+++ comp-final/main.js
@@ -11,48 +11,16 @@ function getStockData(){
     xhr.onreadystatechange = function getStockData (){
         if (xhr.readyState==4){
             if (xhr.status==200){
+                //store response text to local storage to generate all tables
+                var responseText = xhr.responseText;
+                localStorage.setItem("searchResult",responseText);
     
-                //display in console the response text for troubleshooting
+                //print response text to console for troubleshooting
                 console.log("Showing response text:");
                 console.log(xhr.responseText); 
 
-                //set variables for stockdata
-                var stockJson = JSON.parse(xhr.responseText);
-                var firstKey = Object.keys(stockJson)[0];
-                var ticker = stockJson["Global Quote"]["01. symbol"];
-                var currentPrice = stockJson["Global Quote"]["05. price"];
-                var previousClose = stockJson["Global Quote"]["08. previous close"];
-                var tradingDay = stockJson["Global Quote"]["07. latest trading day"];
-
- 
-                
-
-                //validate entry
-                if(firstKey=="Error Message"){
-                    document.getElementById("dataDisplay").innerHTML="Invalid entry";
-
-                }else{
-                 //set table variables
-                 var tbl = document.getElementById("mainDisplay");
-                 var row = tbl.insertRow();
-                 var cell1 = row.insertCell();
-                 var cell2 = row.insertCell();
-                 var cell3 = row.insertCell();
-                 var cell4 = row.insertCell();
-                 cell1.innerHTML = ticker;
-                 cell2.innerHTML = tradingDay;
-                 cell3.innerHTML = currentPrice;
-                 cell4.innerHTML = previousClose;
-                    
-                };                
-                
-    
-                //store JSON string to local storage
-                //var responseText = JSON.stringify(xhr.responseText);
-                //localStorage.setItem("myData",responseText);
-    
-                //display incoming JSON data            
-                //document.getElementById("dataDisplay").innerHTML=responseText;
+                //display results
+                displayResults();
     
             if(xhr.status==404){
                 console.log("File or resource not found");
@@ -60,16 +28,137 @@ function getStockData(){
             }
         }
     };
-    
-    //Personal api key: TXLZQ0VXP5QUYSXN
 
     //trim whitespace and carriage returns from user input
     var symbol = document.getElementById("userInput").value.trim().replace(/[\n\r]/g, '');
 
-    //put trimmed output in url
-    url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="+symbol+"&apikey=TXLZQ0VXP5QUYSXN";
+    //add trimmed output variable 'symbol' in url and send request
+    url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="
+            +symbol+"&apikey=TXLZQ0VXP5QUYSXN";
     xhr.open('get',url,true);
     xhr.send();
+    }
+
+    /* ***** Functions ***** */
+
+    //delete current results table if it exists so that only one table shows after stock search
+    function deleteTable(tableId){
+        if(document.getElementById(tableId) != null){
+            document.getElementById(tableId).remove();
+            console.log("Table deleted");
+        }
+
+    }
+
+    //general create table function for all tables in app
+    function createTable(elementId,tableId,rowId){
+
+        var t = document.createElement('table');
+        t.setAttribute("id", tableId);
+        document.getElementById(elementId).appendChild(t);
+      
+        var r = document.createElement('tr');
+        r.setAttribute("id", rowId);
+        document.getElementById(tableId).appendChild(r);
+      
+        var c = document.createElement('td');
+        var cinput = document.createTextNode("Symbol");
+        c.appendChild(cinput);
+        document.getElementById(rowId).appendChild(c);
+
+        var c2 = document.createElement('td');
+        var c2input = document.createTextNode("Date");
+        c2.appendChild(c2input);
+        document.getElementById(rowId).appendChild(c2);
+
+        var c3 = document.createElement('td');
+        var c3input = document.createTextNode("Current Price");
+        c3.appendChild(c3input);
+        document.getElementById(rowId).appendChild(c3);
+
+        var c4 = document.createElement('td');
+        var c4input = document.createTextNode("Previous Close");
+        c4.appendChild(c4input);
+        document.getElementById(rowId).appendChild(c4);
+
+        var c5 = document.createElement('td');
+        var c5input = document.createTextNode("Action");
+        c5.appendChild(c5input);
+        document.getElementById(rowId).appendChild(c5);
+
+        console.log("Table created");
+
+      }
+
+      //display results from initial request to get stock information
+      function displayResults(){
+        var stockJson = JSON.parse(localStorage.getItem('searchResult'));
+        var firstKey = Object.keys(stockJson)[0];
+        var ticker = stockJson["Global Quote"]["01. symbol"];
+        var currentPrice = stockJson["Global Quote"]["05. price"];
+        var previousClose = stockJson["Global Quote"]["08. previous close"];
+        var tradingDay = stockJson["Global Quote"]["07. latest trading day"];
+
+        if(firstKey=="Error Message"){
+            document.getElementById("dataDisplay").innerHTML="Invalid entry";
+
+        }else{    
+
+        //delete any output table if it exists
+        deleteTable("outputTable");
+         
+        //create the display table for the new stock entered
+        createTable("dataDisplay","outputTable","outputRow");
+
+        //append table with data from api call      
+        var tbl = document.getElementById("outputTable");
+         var row = tbl.insertRow();
+         var cell1 = row.insertCell();
+         var cell2 = row.insertCell();
+         var cell3 = row.insertCell();
+         var cell4 = row.insertCell();
+         var cell5 = row.insertCell();
+         cell1.innerHTML = ticker;
+         cell2.innerHTML = tradingDay;
+         cell3.innerHTML = currentPrice;
+         cell4.innerHTML = previousClose;
+         cell5.innerHTML = "<input type='button' onclick='addToWatchlist()' \
+            value='Add to Watchlist'></input>";
+            
+        };            
+
+      }
+
+ 
+      function addToWatchlist(){
+        //verify if watchlist table exists
+        if(document.getElementById("watchlistTable") == null){
+            createTable("watchlist","watchlistTable","watchlistRow");
+            console.log("Watchlist Table created");          
+        } 
+
+        //set watchlist variables
+        var stockJson = JSON.parse(localStorage.getItem('searchResult'));
+        var ticker = stockJson["Global Quote"]["01. symbol"];
+        var currentPrice = stockJson["Global Quote"]["05. price"];
+        var previousClose = stockJson["Global Quote"]["08. previous close"];
+        var tradingDay = stockJson["Global Quote"]["07. latest trading day"];
+
+        //set table variables
+        var tbl = document.getElementById("watchlistTable");
+        var row = tbl.insertRow();
+        var cell1 = row.insertCell();
+        var cell2 = row.insertCell();
+        var cell3 = row.insertCell();
+        var cell4 = row.insertCell();
+        var cell5 = row.insertCell();
+        cell1.innerHTML = ticker;
+        cell2.innerHTML = tradingDay;
+        cell3.innerHTML = currentPrice;
+        cell4.innerHTML = previousClose;
+        cell5.innerHTML = "<input type='button' onclick='removeFromWatchlist()' \
+           value='Remove from Watchlist'></input>";
+
     }
 
     /*
